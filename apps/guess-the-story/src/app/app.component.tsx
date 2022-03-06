82916dc082d03101e4b898dc8a4f05f54e3fbf7a
William Ghelfi <trumbitta@gmail.com>
2022-03-06 15:23:19 +0000

    refactor(guess-the-story): polish App component a bit

--- apps/guess-the-story/src/app/app.component.tsx
+++ apps/guess-the-story/src/app/app.component.tsx
@@ -6,20 +6,16 @@ import { Route, Redirect } from 'react-router-dom';
 // Libs
 import { PageStories } from '@playroom/guess-the-story/features/stories';
 
-export const App: FC = () => {
-  return (
-    <>
-      <Route exact path="/home">
-        <Redirect to="/stories" />
-      </Route>
-      <Route path="/stories">
-        <PageStories />
-      </Route>
-      <Route exact path="/">
-        <Redirect to={'/home'} />
-      </Route>
-    </>
-  );
-};
-
-export default App;
+export const App = () => (
+  <>
+    <Route exact path="/home">
+      <Redirect to="/stories" />
+    </Route>
+    <Route path="/stories">
+      <PageStories />
+    </Route>
+    <Route exact path="/">
+      <Redirect to={'/home'} />
+    </Route>
+  </>
+);
