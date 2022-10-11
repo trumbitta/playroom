c4608c48d0f3899336e08721c51e94662961c47d
William Ghelfi <trumbitta@gmail.com>
2022-10-11 21:45:51 +0000

    feat(kontra-color): choose random goal cell on startup

--- apps/kontra-color/src/app/app.tsx
+++ apps/kontra-color/src/app/app.tsx
@@ -3,38 +3,46 @@ import { useCallback, useEffect, useRef } from 'react';
 import { init, Sprite, GameLoop, keyPressed, initKeys } from 'kontra';
 
 import { interpolateWarm } from 'd3-scale-chromatic';
-import { interpolate, quantize } from 'd3-interpolate';
+import { quantize } from 'd3-interpolate';
 
 export function App() {
   const canvasRef = useRef<HTMLCanvasElement>(null);
 
-  const heroPositionToCellPosition = ({ x, y }: Position): Position => ({
+  const canvasPositionToCellPosition = ({ x, y }: Position): Position => ({
     x: Math.floor(x / heroWidth),
     y: Math.floor(y / heroHeight),
   });
 
-  const getColorByCellPosition = useCallback(
-    ({
-      x,
-      y,
-      maxCellX,
-      maxCellY,
-    }: Position & { maxCellX: number; maxCellY: number }): string => {
-      const coloredCellPositions = quantize(
-        interpolateWarm,
-        maxCellX * maxCellY
-      );
+  const cellPositionToCanvasPosition = ({ x, y }: Position): Position => ({
+    x: x * heroWidth,
+    y: y * heroHeight,
+  });
 
-      const heroCellIndex = y * maxCellY + x;
-
-      return coloredCellPositions[heroCellIndex];
-    },
+  const getRandomGoalCell = useCallback(
+    ({ x: maxCellX, y: maxCellY }: Position): Position =>
+      cellPositionToCanvasPosition({
+        x: Math.floor(Math.random() * (Math.floor(maxCellX - 1) - 0 + 1) + 0),
+        y: Math.floor(Math.random() * (Math.floor(maxCellY - 1) - 0 + 1) + 0),
+      }),
     []
   );
+
+  const getArrayIndexFromCellPosition = ({
+    x,
+    y,
+    maxCellY,
+  }: Position & {
+    maxCellY: number;
+  }): number => y * maxCellY + x;
 
   useEffect(() => {
     const { canvas } = init(canvasRef.current as HTMLCanvasElement);
     initKeys();
+
+    const { x: maxCellX, y: maxCellY } = canvasPositionToCellPosition({
+      x: canvas.width,
+      y: canvas.height,
+    });
 
     const background = Sprite({
       x: 0,
@@ -52,6 +60,35 @@ export function App() {
       height: heroHeight,
     });
 
+    const goalCell = getRandomGoalCell({ x: maxCellX, y: maxCellY });
+    console.log(goalCell, maxCellX, maxCellY);
+    const goal = Sprite({
+      ...goalCell,
+      color: 'white',
+      width: heroWidth,
+      height: heroHeight,
+    });
+
+    const shiftArray = <T,>(array: T[], howFar: number): T[] =>
+      array.concat(array.splice(0, howFar));
+
+    const orderCellPositionsByGoalCell = (
+      cellPositions: string[],
+      goalCellIndex: number
+    ): string[] => {
+      const distanceFromGoalColor = cellPositions.length - goalCellIndex - 1;
+
+      return shiftArray(cellPositions, distanceFromGoalColor);
+    };
+
+    const coloredCellPositions = orderCellPositionsByGoalCell(
+      quantize(interpolateWarm, maxCellX * maxCellY),
+      getArrayIndexFromCellPosition({
+        ...canvasPositionToCellPosition({ ...goalCell }),
+        maxCellY,
+      })
+    );
+
     const loop = GameLoop({
       update: function () {
         moveHero({
@@ -67,38 +104,32 @@ export function App() {
         background.update();
         hero.update();
 
-        const { x: maxCellX, y: maxCellY } = heroPositionToCellPosition({
-          x: canvas.width,
-          y: canvas.height,
-        });
-
-        const heroCellPosition = heroPositionToCellPosition({
+        const heroCellPosition = canvasPositionToCellPosition({
           x: hero.x,
           y: hero.y,
         });
 
-        background.color = getColorByCellPosition({
-          x: heroCellPosition.x,
-          y: heroCellPosition.y,
-          maxCellX,
+        const index = getArrayIndexFromCellPosition({
+          ...heroCellPosition,
           maxCellY,
         });
+        background.color = coloredCellPositions[index];
       },
       render: function () {
         background.render();
+        goal.render();
         hero.render();
       },
     });
 
     loop.start();
-  }, [getColorByCellPosition]);
+  }, [getRandomGoalCell]);
 
   const getXByPercentage = (xPercent: number) =>
     ((300 - heroWidth) * xPercent) / 100;
   const getYByPercentage = (yPercent: number) =>
     ((150 - heroHeight) * yPercent) / 100;
 
-  // TODO: explore making this immutable
   const moveHero = ({
     hero,
     bounds,
